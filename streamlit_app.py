18a400eaeae0b0097003771732514e1ccd8f453c
adigiovanni97 <adigiovanni1997@gmail.com>
2024-06-11 02:30:12 +0000

    Init commit
    Missing re-computing accum when date filters change.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,62 +2,23 @@ import streamlit as st
 import pandas as pd
 import math
 from pathlib import Path
+from txt_parser import parse_file
+from datetime import datetime
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
-    page_title='GDP Dashboard',
+    page_title='GDP Boenas',
     page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
 )
 
 # -----------------------------------------------------------------------------
 # Declare some useful functions.
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
+#@st.cache_data
+def get_msg_data():
+    return parse_file()
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
-
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
-
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
-
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
-
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
-
-gdp_df = get_gdp_data()
+df = get_msg_data()
 
 # -----------------------------------------------------------------------------
 # Draw the actual page
@@ -75,77 +36,80 @@ But it's otherwise a great (and did I mention _free_?) source of data.
 ''
 ''
 
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
+'''Select a date interval: '''
+from_datetime, to_datetime = st.date_input("Select a date interval", (df['datetime'].min(), df['datetime'].max()))
+from_datetime = datetime(from_datetime.year, from_datetime.month, from_datetime.day)
+to_datetime = datetime(to_datetime.year, to_datetime.month, to_datetime.day)
 
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
+users = df['sender'].unique()
 
-countries = gdp_df['Country Code'].unique()
+if not len(users):
+    st.warning("Select at least one user")
 
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
+selected_users = st.multiselect(
+    'Which users would you like to view?',
+    users,
+    users)
 
 ''
 ''
 ''
+print(df)
 
 # Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
+filtered_df = df[
+    (df['sender'].isin(selected_users))
+    & (df['datetime'] <= to_datetime)
+    & (from_datetime <= df['datetime'])
 ]
 
-st.header('GDP over time', divider='gray')
+st.header('Time series', divider='gray')
 
 ''
+''
+
+print(filtered_df)
 
 st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
+    filtered_df,
+    x='datetime',
+    y='accum',
+    color='sender',
 )
 
 ''
 ''
 
-
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
-
-st.header(f'GDP in {to_year}', divider='gray')
-
+''
 ''
 
-cols = st.columns(4)
 
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
+# first_year = gdp_df[gdp_df['Year'] == from_year]
+# last_year = gdp_df[gdp_df['Year'] == to_year]
 
-    with col:
-        first_gdp = first_year[gdp_df['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[gdp_df['Country Code'] == country]['GDP'].iat[0] / 1000000000
+# st.header(f'GDP in {to_year}', divider='gray')
 
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
+# ''
 
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+# cols = st.columns(4)
+
+# for i, country in enumerate(selected_countries):
+#     col = cols[i % len(cols)]
+
+#     with col:
+#         first_gdp = first_year[gdp_df['Country Code'] == country]['GDP'].iat[0] / 1000000000
+#         last_gdp = last_year[gdp_df['Country Code'] == country]['GDP'].iat[0] / 1000000000
+
+#         if math.isnan(first_gdp):
+#             growth = 'n/a'
+#             delta_color = 'off'
+#         else:
+#             growth = f'{last_gdp / first_gdp:,.2f}x'
+#             delta_color = 'normal'
+
+#         st.metric(
+#             label=f'{country} GDP',
+#             value=f'{last_gdp:,.0f}B',
+#             delta=growth,
+#             delta_color=delta_color
+#         )
---
+++ txt_parser.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+import pandas as pd
+from pathlib import Path
+import re
+import streamlit as st
+
+DATETIME_RE = r"(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<year>\d{2}), (?P<hour>\d{1,2}):(?P<minute>\d{2})\s(?P<meridian>[AP]M)"
+
+MESSAGE_RE = r"(?P<datetime>\d+\/\d+\/\d{2}, \d+:\d{2}\s[A|P]M) - (?P<sender>[\w|\s]+): (?P<message>.+)"
+DATA_PATH = Path(__file__).parent/'data/datos.txt'
+
+def _parse_datetime(date_message):
+  match = re.match(DATETIME_RE, date_message)
+  if match:
+    date_dict = match.groupdict()
+    month, day, year = int(date_dict["month"]), int(date_dict["day"]), int(date_dict["year"])
+
+    # los forros de whatsapp exportan el anio con dos digitos
+    year += 2000
+    hour, minute = int(date_dict["hour"]), int(date_dict["minute"])
+    meridian = date_dict["meridian"]
+
+    # Convert hour to 24-hour format based on meridian
+    if meridian == "PM" and hour < 12:
+        hour += 12
+
+    # Create datetime object
+    parsed_datetime = datetime(year, month, day, hour, minute)
+
+    return parsed_datetime
+
+
+def parse_file(filename=DATA_PATH):
+    events = []
+    senders_accum = {}
+    with open(DATA_PATH, 'r') as file:  # 'r' for read mode
+        for line in file:
+            match = re.match(MESSAGE_RE, line)
+            if match:
+                sender = match.group("sender")
+
+                event = {
+                    "datetime": _parse_datetime(match.group("datetime")),
+                    "sender": sender,
+                    "message": match.group("message")
+                }
+
+                if re.search(r"^\*?\d+\*?(\s.*)?$", event["message"]) or re.search(r"^\.$", event["message"]):
+                    if sender not in senders_accum:
+                        senders_accum[sender] = 0
+                    senders_accum[sender] += 1
+                    event["accum"] = senders_accum[sender]
+                    events.append(event)
+    return pd.DataFrame(events, columns=['datetime', 'sender', 'message', 'accum'])
